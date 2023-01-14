97ba37612b7c43bc0bdf1556e2a497a505a46f3e
Thetourman <107192370+Thetourman@users.noreply.github.com>
2023-01-14 22:06:43 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,24 +1,15 @@
-h,m = map(int, input().split())
+h,m = map(int,input().split())
 n = int(input())
-ht = n//60
-mt = n%60
 
-if n>59:
-  h+=ht
-  m+=mt
-  if h >= 24:
-    h = h%24
-  if m >= 60:
-    h += 1
-    m -= 60
-    if h >= 24:
-      h = h%24
-else:
-  m += n
-  if m>59:
-    h += 1
-    m -= 60
-    if h>=24:
-       h = h%24
+h = h+n//60
+m = m+n%60
+
+if h>23:
+  h-=24
+if m>59:
+  h+=1
+  m-=60
+  if h>23:
+    h-=24
 
 print(h,m)
